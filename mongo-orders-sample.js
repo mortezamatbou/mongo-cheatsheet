d8054c56d106d15a6030dbee45537889f496f98e
morteza <mortezamatbou91@gmail.com>
2024-01-06 16:54:14 +0330

    refactor orders. add users collection

--- mongo-orders-sample.js
+++ mongo-orders-sample.js
@@ -1,3 +1,18 @@
+db.users.insertMany([
+    {
+        _id: 1,
+        username: 'mori',
+        email: 'mori@lobdown.com',
+        registerDate: '2023-01-06 16:53:25',
+        tmpStrategies: ['Ichimoku RSI', 'Price Action', 'RTM', 'ICT'],
+        tmpReadyChecklist: ['', '', ''],
+        tmpMistakes: ['FOMO', 'Overtrade', 'No SL'],
+        tmpEmotions: ['Fear', 'Greedy', 'Tired', 'Angry'],
+        tmpEndDayQuestions: ['Did i follow my rules?', 'Did i hit my target?', 'Did i learn something new?']
+    },
+]);
+
+
 db.orders.insertMany([
     {
         orderId: 1,
@@ -11,76 +26,29 @@ db.orders.insertMany([
         info: {
             ep: {
                 1: { price: 0.6, amount: 250, status: 'filled' },
-                2: { price: 0.55, amount: 501, status: 'pending' }
+                2: { price: 0.55, amount: 501, status: 'filled' }
             },
             sl: {
                 1: { price: 0.5, volume: 50, status: 'pending' },
                 2: { price: 0.45, volume: 50, status: 'pending' },
             },
+            rf: 0,
             cls: {}
         },
         date: '2023-01-03',
         time: '19:08',
-        creationDate: '2023-01-03 20:01',
-        closeDate: '2023-01-03 22:47',
+        creationDateTime: '2023-01-03 20:01',
+        closeDateTime: '2023-01-03 22:47',
         status: 'OPEN',
         pnl: {
             amount: 425.066,
             profit_p: 10,
             profit: 42.5,
             loss: 0
-        }
-    },
-    {
-        orderId: 2,
-        userId: 2,
-        symbol: 'ADAUSDT',
-        date: '2023-01-04',
-        time: '15:24',
-        side: 'Buy',
-        market: 'Future',
-        amount: 415.5,
-        orderType: 'LIMIT',
-        info: {
-            ep: {
-                1: { vol: 50, prc: 0.51, status: 'filled' },
-                2: { vol: 50, prc: 0.554, status: 'pending' }
-            },
-            sl: {
-                1: { vol: 50, prc: 0.61, status: 'pending' },
-                2: { vol: 50, prc: 0.688, status: 'pending' },
-            },
-            close: {
-                // 1: { vol: 100, prc: 0.6, status: 'filled' },
-            }
         },
-        status: 'open',
+        timeframe: "4h",
+        strategy: "Ichimoku"
     },
-    {
-        orderId: 3,
-        userId: 2,
-        symbol: 'ETHUSDT',
-        date: '2023-01-04',
-        time: '10:15',
-        side: 'Buy',
-        market: 'Spot',
-        amount: 415.5,
-        orderType: 'StopLimit',
-        info: {
-            ep: {
-                1: { prc: 0.51, status: 'filled' },
-                2: { prc: 0.554, status: 'pending' }
-            },
-            sl: {
-                1: { prc: 0.61, status: 'pending' },
-                2: { prc: 0.688, status: 'pending' },
-            },
-            close: {
-                // 1: { vol: 100, prc: 0.6, status: 'filled' },
-            }
-        },
-        status: 'open',
-    }
 ]);
 
 
